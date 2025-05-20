5b552df93a54d2ce48af0f6e9cdd9e64f96bd13f
Davi Ricardo <davir6229@gmail.com>
2025-05-20 00:27:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# site-brasileirao
+Site sobre o Brasileirão 2025
---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Filtro por nome
+const searchInput = document.getElementById("search");
+const cards = document.querySelectorAll(".card-time");
+
+searchInput.addEventListener("input", function () {
+  const searchValue = this.value.toLowerCase();
+
+  cards.forEach(card => {
+    const nomeTime = card.querySelector("h2").textContent.toLowerCase();
+    card.style.display = nomeTime.includes(searchValue) ? "block" : "none";
+  });
+});
